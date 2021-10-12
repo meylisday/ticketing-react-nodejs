35033a3c4cee5db7546e0e483557566423126795
meylisday <victory.sett@gmail.com>
2021-10-12 10:52:52 +0300

    Update k8s configs, add ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { json } from 'body-parser';
 
-const app = express(); 
+const app = express();
+app.use(express.json());
 
-app.use(json());
+app.get('/api/users/currentuser', (req, res) => {
+ res.send('Hi there!');
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
